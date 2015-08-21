c6cb936b407a84f8b96957caebf3f6e4e43148a2
Sam Jordan <samuelfjordan@gmail.com>
2015-08-21 17:29:37 +0300

    change width code, profile pic

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,17 @@
 $(document).ready(setRunnerHeight);
+$(document).ready(setRunnerWidth);
 // $(document).scroll(setRunnerHeight);
 window.onresize = setRunnerHeight;
+window.onresize = setRunnerWidth;
 
 function setRunnerHeight() {
     var distToFooter=$('#footer').offset().top;
     $('#runner').css('height',(distToFooter-100)+'px');
+}
+
+function setRunnerWidth() {
+    if ($(document).width() < 650) {
+        $('#runner').css('left','25px');
+        $('body').css('padding-left','45px');
+    }
 }
